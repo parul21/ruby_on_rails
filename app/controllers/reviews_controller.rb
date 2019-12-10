b01c99b6cf340c667e7bb3a901806c200f0fd225
parul21 <parullucky21@gmail.com>
2019-12-10 15:59:20 +0530

    add edit and update working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,10 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user! 
+  before_action :set_movie
 
   # GET /reviews
   # GET /reviews.json
-  def index
-    @reviews = Review.all
-  end
-
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
 
   # GET /reviews/new
   def new
@@ -27,10 +20,10 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-
+    @review.movie_id = params[:movie_id]
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to movie_path(params[:movie_id]), notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -43,8 +36,8 @@ class ReviewsController < ApplicationController
   # PATCH/PUT /reviews/1.json
   def update
     respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+      if @review.update(update_params)
+        format.html { redirect_to movie_path(params[:movie_id]), notice: 'Review was successfully updated.' }
         format.json { render :show, status: :ok, location: @review }
       else
         format.html { render :edit }
@@ -58,19 +51,25 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to root_path, notice: 'Review was successfully destroyed.' }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_review
-      @review = Review.find(params[:id])
+      @review = Review.find_by_id(params[:id])
     end
-
+    def set_movie
+      @movie =  Movie.find_by_id(params[:id])
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:rating, :comment)
+      params.permit(:rating, :comment, :movie_id)
+    end
+     def update_params
+      @a=params[:review]
+      b=@a.merge(review_params)
+      b.permit(:rating, :comment, :movie_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :reviews
+  
   devise_for :users
-  resources :movies
+
+  resources :movies do
+  	resources :reviews, except: [:show, :index] do 
+  		collection do
+  			post 'movie/:id/review/create ', to: "reviews#create"
+  		end
+  	end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'movies#index'
 end
